5b79cd6e77c0f9180be15406ff83155fa1317f90
Ezana Kebede <ezanakebede10@gmail.com>
2023-02-28 23:12:29 -0500

    Finished writing TestMapGetLoc.java

--- Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
+++ Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
@@ -1,11 +1,29 @@
 package pacman;
 import java.io.*;
+import java.util.HashSet;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
 import junit.framework.*;
+import pacman.Map.Type;
+
 import java.awt.Color;
 
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
+    Map map = new Map(0);
+    map.add("first add", new Location(2, 3), null, Type.PACMAN);
+    map.add("second add", new Location(2, 3), null, Type.GHOST);
+    map.add("third add", new Location(5, 4), null, Type.COOKIE);
+    map.add("fourth add", new Location(8, 3), null, Type.GHOST);
+    map.add("fifth add", new Location(2, 3), null, Type.COOKIE);
+    HashSet<Type> locationSet = map.getLoc(new Location(2, 3));
+    for (Type s : locationSet) {
+      System.out.println(s.toString());
+    }
     return;
   }
+
 }
