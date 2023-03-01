0e76cdd035f696df323ee2b20ba176a11e37084d
Kanishk Chinna <kanishkchinna@gmail.com>
2023-03-01 16:49:01 -0500

    added tests

--- Projects/P2/app/src/test/java/pacman/TestMapMove.java
+++ Projects/P2/app/src/test/java/pacman/TestMapMove.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+    PacMan pacman = frame.addPacMan(new Location(2, 4));
+    assertTrue(map.move("pacman", new Location (2,5), Map.Type.PACMAN))
     return;
   }
 }
