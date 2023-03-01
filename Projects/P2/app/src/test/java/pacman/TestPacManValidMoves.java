0fbf1cab6d281c7ac13400bfc902d2a41559ea8e
Kanishk Chinna <kanishkchinna@gmail.com>
2023-03-01 17:05:43 -0500

    added test

--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+    PacMan pacman = frame.addPacMan(new Location(9, 11));
+    assertEquals(pacman.get_valid_moves(), {(9,12), (9,10), (10,11), (11, 11)})
     return;
   }
 }
