5bb0a57700afb0a45e5d9179813f427b7bad66d5
Ashley Rider <ashleyrider2001@gmail.com>
2023-03-01 15:55:32 -0500

    testing and a syntax error fix in ghost-attack

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -26,7 +26,7 @@ public class Ghost {
   }
 
   public boolean attack() {
-    if(is_pacman_in_range){
+    if(is_pacman_in_range()){
       return myMap.attack();
     }
     return false;
--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,6 +6,21 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
+    //Creating A Map
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+    //Creating Player
+    PacMan pacman1 = frame.addPacMan(new Location(4,5)); //Creates PacMan at same location as a ghost
+
+    //Creating Ghosts
+    Ghost ghost1 = frame.addGhost(new Location(4,5), "ghost1", Color.cyan);
+    Ghost ghost2 = frame.addGhost(new Location(2,3), "ghost2", Color.pink);
+
+    // Assert that cookies are removed when eaten, and pacman cannot eat a cookie that isn't there
+    assertEquals(ghost1.attack(), frame.getMap().attack());
+    // Assert that the cookie counter is not being incremented when no cookie is eaten
+    assertEquals(ghost2.attack(), false);
+
     return;
   }
 }
