024f7e2a2be3ddb45aafaa393887955867fe26bf
Kanishk Chinna <kanishkchinna@gmail.com>
2023-03-01 17:09:48 -0500

    added tests

--- Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+    Ghost ghost = frame.addGhost(new Location(9, 11));
+    assertEquals(ghost.get_valid_moves(), {(9,12), (9,10), (10,11), (11, 11)})
     return;
   }
 }
